fc61c8435ed2fa425ce293cf8fc7c6af3e8fc590
ahmed2026-abdelhalem <ahmedgithub2026@gmail.com>
2026-07-29 20:10:04 +0300

    starting in project, i mapping database with java project and put important dependency

---
+++ src/test/java/com/library/fullstacktaskmanagement/FullStackTaskManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.fullstacktaskmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackTaskManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
