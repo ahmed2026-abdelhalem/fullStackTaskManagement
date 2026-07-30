b8a40d306744b1552643d212ea72ce4721c995a7
ahmed2026-abdelhalem <ahmedgithub2026@gmail.com>
2026-07-30 16:26:10 +0300

    finished created repository

---
+++ src/main/java/com/library/fullstacktaskmanagement/repository/TaskRepository.java
@@ -0,0 +1,18 @@
+package com.library.fullstacktaskmanagement.repository;
+
+import com.library.fullstacktaskmanagement.entity.Task;
+import com.library.fullstacktaskmanagement.entity.enums.TaskStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+
+    List<Task> findByUserId(Long userId);
+
+    List<Task> findByUserIdAndStatus(Long userId, TaskStatus status);
+
+}
